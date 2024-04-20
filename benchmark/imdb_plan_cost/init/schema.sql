f6586e45aff8f771eadf757622b95a2ca1e257fd
Tsihan <2453939195@qq.com>
2024-04-20 20:03:58 +0000

    add bitmap to help hash join, and some building sql for idb

--- benchmark/imdb_plan_cost/init/schema.sql
+++ benchmark/imdb_plan_cost/init/schema.sql
@@ -1,5 +1,88 @@
+CREATE TABLE name (
+    id integer NOT NULL PRIMARY KEY,
+    name character varying(106) NOT NULL,
+    imdb_index character varying(12),
+    imdb_id integer,
+    gender character varying(1),
+    name_pcode_cf character varying(5),
+    name_pcode_nf character varying(5),
+    surname_pcode character varying(5),
+    md5sum character varying(32)
+);
+
+CREATE TABLE char_name (
+    id integer NOT NULL PRIMARY KEY,
+    name character varying(478) NOT NULL,
+    imdb_index character varying(12),
+    imdb_id integer,
+    name_pcode_nf character varying(5),
+    surname_pcode character varying(5),
+    md5sum character varying(32)
+);
+
+CREATE TABLE comp_cast_type (
+    id integer NOT NULL PRIMARY KEY,
+    kind character varying(32) NOT NULL
+);
+
+CREATE TABLE company_name (
+    id integer NOT NULL PRIMARY KEY,
+    name character varying(200) NOT NULL,
+    country_code character varying(255),
+    imdb_id integer,
+    name_pcode_nf character varying(5),
+    name_pcode_sf character varying(5),
+    md5sum character varying(32)
+);
+
+CREATE TABLE company_type (
+    id integer NOT NULL PRIMARY KEY,
+    kind character varying(32) NOT NULL
+);
+
+CREATE TABLE info_type (
+    id integer NOT NULL PRIMARY KEY,
+    info character varying(32) NOT NULL
+);
+
+CREATE TABLE keyword (
+    id integer NOT NULL PRIMARY KEY,
+    keyword character varying(74) NOT NULL,
+    phonetic_code character varying(5)
+);
+
+CREATE TABLE kind_type (
+    id integer NOT NULL PRIMARY KEY,
+    kind character varying(15) NOT NULL
+);
+
+CREATE TABLE link_type (
+    id integer NOT NULL PRIMARY KEY,
+    link character varying(32) NOT NULL
+);
+
+CREATE TABLE role_type (
+    id integer NOT NULL PRIMARY KEY,
+    role character varying(32) NOT NULL
+);
+
+CREATE TABLE title (
+    id integer NOT NULL PRIMARY KEY,
+    title character varying(334) NOT NULL,
+    imdb_index character varying(12),
+    kind_id integer NOT NULL REFERENCES kind_type(id),
+    production_year integer,
+    imdb_id integer,
+    phonetic_code character varying(5),
+    episode_of_id integer,
+    season_nr integer,
+    episode_nr integer,
+    series_years character varying(49),
+    md5sum character varying(32)
+);
+
 CREATE TABLE aka_name (
-    id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY REFERENCES name(id),
     person_id integer NOT NULL,
     name character varying(218) NOT NULL,
     imdb_index character varying(12),
@@ -10,7 +93,7 @@ CREATE TABLE aka_name (
 );
 
 CREATE TABLE aka_title (
-    id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY,
     movie_id integer NOT NULL,
     title character varying(553) NOT NULL,
     imdb_index character varying(12),
@@ -25,146 +108,63 @@ CREATE TABLE aka_title (
 );
 
 CREATE TABLE cast_info (
-    id integer NOT NULL,
-    person_id integer NOT NULL,
-    movie_id integer NOT NULL,
-    person_role_id integer,
+    id integer NOT NULL PRIMARY KEY,
+    person_id integer NOT NULL REFERENCES aka_name(id),
+    movie_id integer NOT NULL REFERENCES title(id),
+    person_role_id integer REFERENCES char_name(id),
     note character varying(992),
     nr_order integer,
-    role_id integer NOT NULL
-);
-
-CREATE TABLE char_name (
-    id integer NOT NULL,
-    name character varying(478) NOT NULL,
-    imdb_index character varying(12),
-    imdb_id integer,
-    name_pcode_nf character varying(5),
-    surname_pcode character varying(5),
-    md5sum character varying(32)
-);
-
-CREATE TABLE comp_cast_type (
-    id integer NOT NULL,
-    kind character varying(32) NOT NULL
-);
-
-CREATE TABLE company_name (
-    id integer NOT NULL,
-    name character varying(200) NOT NULL,
-    country_code character varying(255),
-    imdb_id integer,
-    name_pcode_nf character varying(5),
-    name_pcode_sf character varying(5),
-    md5sum character varying(32)
-);
-
-CREATE TABLE company_type (
-    id integer NOT NULL,
-    kind character varying(32) NOT NULL
+    role_id integer NOT NULL REFERENCES role_type(id)
 );
 
 CREATE TABLE complete_cast (
-    id integer NOT NULL,
-    movie_id integer,
-    subject_id integer NOT NULL,
-    status_id integer NOT NULL
-);
-
-CREATE TABLE info_type (
-    id integer NOT NULL,
-    info character varying(32) NOT NULL
-);
-
-CREATE TABLE keyword (
-    id integer NOT NULL,
-    keyword character varying(74) NOT NULL,
-    phonetic_code character varying(5)
-);
-
-CREATE TABLE kind_type (
-    id integer NOT NULL,
-    kind character varying(15) NOT NULL
-);
-
-CREATE TABLE link_type (
-    id integer NOT NULL,
-    link character varying(32) NOT NULL
+    id integer NOT NULL PRIMARY KEY,
+    movie_id integer REFERENCES title(id),
+    subject_id integer NOT NULL REFERENCES comp_cast_type(id),
+    status_id integer NOT NULL REFERENCES comp_cast_type(id)
 );
 
 CREATE TABLE movie_companies (
-    id integer NOT NULL,
-    movie_id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY,
+    movie_id integer NOT NULL REFERENCES title(id),
     company_id integer NOT NULL,
     company_type_id integer NOT NULL,
     note character varying(208)
 );
 
 CREATE TABLE movie_info (
-    id integer NOT NULL,
-    movie_id integer NOT NULL,
-    info_type_id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY,
+    movie_id integer NOT NULL REFERENCES title(id),
+    info_type_id integer NOT NULL REFERENCES info_type(id),
     info character varying(8000) NOT NULL,
     note character varying(387)
 );
 
 CREATE TABLE movie_info_idx (
-    id integer NOT NULL,
-    movie_id integer NOT NULL,
-    info_type_id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY,
+    movie_id integer NOT NULL REFERENCES title(id),
+    info_type_id integer NOT NULL REFERENCES info_type(id),
     info character varying(10) NOT NULL,
     note character varying(1)
 );
 
 CREATE TABLE movie_keyword (
-    id integer NOT NULL,
-    movie_id integer NOT NULL,
-    keyword_id integer NOT NULL
+    id integer NOT NULL PRIMARY KEY,
+    movie_id integer NOT NULL REFERENCES title(id),
+    keyword_id integer NOT NULL REFERENCES keyword(id)
 );
 
 CREATE TABLE movie_link (
-    id integer NOT NULL,
-    movie_id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY,
+    movie_id integer NOT NULL REFERENCES title(id),
     linked_movie_id integer NOT NULL,
-    link_type_id integer NOT NULL
-);
-
-CREATE TABLE name (
-    id integer NOT NULL,
-    name character varying(106) NOT NULL,
-    imdb_index character varying(12),
-    imdb_id integer,
-    gender character varying(1),
-    name_pcode_cf character varying(5),
-    name_pcode_nf character varying(5),
-    surname_pcode character varying(5),
-    md5sum character varying(32)
+    link_type_id integer NOT NULL REFERENCES link_type(id)
 );
 
 CREATE TABLE person_info (
-    id integer NOT NULL,
-    person_id integer NOT NULL,
-    info_type_id integer NOT NULL,
+    id integer NOT NULL PRIMARY KEY,
+    person_id integer NOT NULL REFERENCES name(id),
+    info_type_id integer NOT NULL REFERENCES info_type(id),
     info text NOT NULL,
     note character varying(430)
-);
-
-CREATE TABLE role_type (
-    id integer NOT NULL,
-    role character varying(32) NOT NULL
-);
-
-CREATE TABLE title (
-    id integer NOT NULL,
-    title character varying(334) NOT NULL,
-    imdb_index character varying(12),
-    kind_id integer NOT NULL,
-    production_year integer,
-    imdb_id integer,
-    phonetic_code character varying(5),
-    episode_of_id integer,
-    season_nr integer,
-    episode_nr integer,
-    series_years character varying(49),
-    md5sum character varying(32)
 );
